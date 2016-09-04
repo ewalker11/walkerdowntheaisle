b2b17f382c13f5dc7972e689946d364b6dde6ca1
Eliot Walker <eliot@lyft.com>
2016-09-04 14:54:17 -0700

    Building out more pages

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
+import glob
+import json
+import os
+
 from flask import Flask, render_template
 from jinja2 import TemplateNotFound
+
+PAGE_DATA = {}
+
+for filepath in glob.glob('data/*'):
+    page = os.path.split(filepath)[1].split('.')[0]
+    with open(filepath, 'r') as f:
+        PAGE_DATA.update({page: json.load(f)})
 
 
 app = Flask('walkerdowntheaisle')
 
 
 @app.errorhandler(404)
-def page_not_found(e):
+def page_not_found(_):
     return render_template('notfound.html'), 404
 
 
 @app.errorhandler(TemplateNotFound)
-def template_not_found(e):
+def template_not_found(_):
     return render_template('notfound.html'), 404
 
 
@@ -22,7 +33,7 @@ def index():
 
 @app.route('/<page>')
 def router(page):
-    return render_template('{}.html'.format(page))
+    return render_template('{}.html'.format(page), **PAGE_DATA.get(page, {}))
 
 
 if __name__ == '__main__':
