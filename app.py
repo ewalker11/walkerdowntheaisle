78c60909f6d9877cee742727c6223388bdb57a22
Eliot Walker <eliot@lyft.com>
2016-09-04 13:31:23 -0700

    Simplified approach, use skeleton css

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+from jinja2 import TemplateNotFound
 
-from walkerdowntheaisle.routes.admin import blueprint as admin_blueprint
-from walkerdowntheaisle.routes.index import blueprint as index_blueprint
 
 app = Flask('walkerdowntheaisle')
 
-app.register_blueprint(index_blueprint)
-app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('notfound.html'), 404
+
+
+@app.errorhandler(TemplateNotFound)
+def template_not_found(e):
+    return render_template('notfound.html'), 404
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/<page>')
+def router(page):
+    return render_template('{}.html'.format(page))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- walkerdowntheaisle/models/person.py
+++
@@ -1,7 +0,0 @@
-class Person(object):
-
-    def __init__(self, name=None, title=None, bio=None, photo_url=None):
-        self.name = name
-        self.bio = bio
-        self.title = title
-        self.photo_url = photo_url
