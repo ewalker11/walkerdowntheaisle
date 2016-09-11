1fe534758942199dd695e5701652d79abf908f6e
Eliot Walker <eliot@lyft.com>
2016-09-10 23:11:59 -0700

    Add form validation

--- static/js/rsvp.js
+++ static/js/rsvp.js
@@ -1,11 +1,41 @@
 Zepto(function($){
   $('#num_guests').on('change', function(e){
-    console.log(e.target.value);
     while ($('.rsvp_row:not(.hidden-rsvp-row)').length < e.target.value) {
       $('.hidden-rsvp-row').first().removeClass('hidden-rsvp-row')
     }
     while ($('.rsvp_row:not(.hidden-rsvp-row)').length > e.target.value) {
       $('.rsvp_row:not(.hidden-rsvp-row)').last().addClass('hidden-rsvp-row')
+    }
+  });
+
+  $('input').on('input', function(e) {
+    $(e.target).removeClass('error');
+  });
+  
+  $('#rsvp-form').on('submit', function(e){
+    var erred = false;
+    $('.rsvp_row:not(.hidden-rsvp-row)').each(function(idx, elem) {
+      var firstName = $(elem).find('#first_name');
+      var lastName = $(elem).find('#last_name');
+
+      if ($(elem).find('.attending:checked').empty()) {
+        $(elem).find('.attending').addClass('error');
+        erred = true;
+      }
+
+      if (!firstName.val()) {
+        firstName.addClass('error');
+        erred = true;
+      }
+
+      if (!lastName.val()) {
+        lastName.addClass('error');
+        erred = true;
+      }
+    });
+
+    if (erred) {
+      e.preventDefault();
     }
   });
 });
