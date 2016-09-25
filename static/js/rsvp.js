70b814a2ead15422fda4ceca54dfc4da6fc8545e
Eliot Walker <eliot@lyft.com>
2016-09-25 15:17:14 -0700

    More tweaks

--- static/js/rsvp.js
+++ static/js/rsvp.js
@@ -1,11 +1,11 @@
 Zepto(function($){
   $('#num_guests').on('change', function(e){
-    while ($('.rsvp_row:not(.hidden)').length < e.target.value) {
+    while ($('.rsvp-row:not(.hidden)').length < e.target.value) {
       var hiddenRow = $('.hidden').first();
       hiddenRow.removeClass('hidden').addClass('visible');
     }
-    while ($('.rsvp_row:not(.hidden)').length > e.target.value) {
-      var hiddenRow = $('.rsvp_row:not(.hidden)').last();
+    while ($('.rsvp-row:not(.hidden)').length > e.target.value) {
+      var hiddenRow = $('.rsvp-row:not(.hidden)').last();
       hiddenRow.removeClass('visible').addClass('hidden');
     }
   });
@@ -16,10 +16,10 @@ Zepto(function($){
   
   $('#rsvp-form').on('submit', function(e){
     var erred = false;
-    $('.rsvp_row:not(.hidden)').each(function(idx, elem) {
+    $('.rsvp-row:not(.hidden)').each(function(idx, elem) {
       var firstName = $(elem).find('#first_name');
       var lastName = $(elem).find('#last_name');
-
+e
       if ($(elem).find('.attending:checked').length == 0) {
         $(elem).find('.attending').addClass('error');
         console.log('shit not set');
